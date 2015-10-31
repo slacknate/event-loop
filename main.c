0940e7e85329daa937e50e404cda8a0b527ee991
Nathaniel Duarte <slacknate@gmail.com>
2015-10-31 16:19:52 -0400

    main.c: example file stuff

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
+#include <stdio.h>
+#include "loop/loop.h"
+
+
+void *foo(void *_) {
+
+    SleepEx(2500, 1);
+    printf("O'hallo!\n");
+    return NULL;
+}
+
+
+void *bar(void *_) {
+
+    SleepEx(3000, 1);
+    printf("Howdy!\n");
+    return NULL;
+}
 
 
 int main(int argc, char *argv[]) {
 
+    schedule(&foo);
+    await(&bar);
     return 0;
 }
